ecf9a2d5a6df69649da7ec276f25e0b2a80edc65
Copy Name <fakeeemail@copy.com>
2024-08-22 19:15:58 +0200

    here will be conflict

--- Person.java
+++ Person.java
@@ -1,6 +1,7 @@
 public class Person {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Here will be conflict");
         System.out.println("Next content to push");
     }
 }
