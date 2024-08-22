6a56d7909397dda8d5deac888069e32ca64126fd
Copy Name <fakeeemail@copy.com>
2024-08-22 19:25:02 +0200

    Deleted 2 lines

--- Person.java
+++ Person.java
@@ -2,7 +2,6 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Here will be conflict");
-        System.out.println("Next content to push");
-        System.out.println("content to push from inteliji");
+
     }
 }
